b5105487a505489291c6ed5f9ab8876d8985ac8d
leo <leo.kalshteyn@gmail.com>
2021-01-12 17:29:25 -0800

    add card component

---
+++ src/components/Card.tsx
@@ -0,0 +1,12 @@
+import _ from 'lodash';
+import React, { useState } from 'react';
+
+import * as Models from './../models';
+import './Card.scss';
+
+
+const Card = (props: Props) => {
+
+};
+
+export default Card;
