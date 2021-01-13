825c82ae37ed9a78c247434ba0d24c36f6d4ea28
leo <leo.kalshteyn@gmail.com>
2021-01-12 17:42:20 -0800

    set initial state and lowdash get results

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,8 +4,29 @@ import React, { useState } from 'react';
 import * as Models from './../models';
 import './Card.scss';
 
+interface Props {
+  quizQuestions?: Models.QuizResponse;
+}
 
 const Card = (props: Props) => {
+  const quizQuestions = _.get(props, 'quizQuestions.results', []);
+  const initialState = {
+    currentIndex: 0,
+    score: 0,
+    showFinished: false,
+    answered: false,
+    selectedOption: '',
+    revealAnswer: '',
+  };
+
+  const [state, setState] = useState(initialState);
+
+  const {
+    currentIndex,
+    score,
+    revealAnswer,
+    selectedOption,
+  } = state;
 
 };
 
